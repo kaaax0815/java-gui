42caca1cb246bdd8dc4c3408802a420e13307c1f
kaaax0815 <999999bst@gmail.com>
2023-02-13 18:27:05 +0100

    swing: Initialize Project

--- swing/src/main/java/de/kaaaxcreators/swing/App.java
+++ swing/src/main/java/de/kaaaxcreators/swing/App.java
@@ -1,13 +1,23 @@
 package de.kaaaxcreators.swing;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.swing.*;
+
+public class App {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Swing App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+ 
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main( String[] args ) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
     }
 }
